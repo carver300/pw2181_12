ff2d7ce15921b7477a260c5eb855ae24eadb2f97
carver300 <penunurisantiago@gmail.com>
2018-05-22 00:51:33 -0500

    PaseLista punto 1 terminado

--- PaseLista/js/index.js
+++ PaseLista/js/index.js
@@ -1,6 +1,33 @@
-$('#btnIngresar').on('click',mensaje);
+var $ = require('jquery');
 
-function mensaje()
+
+var sUsuario;
+var sContraseña;
+var bRespuesta;
+var sUsuarioValida;
+var sPeriodo;
+
+var inicia = function()
 {
-    alert("Hola");
+   var validarLogueo = function()
+   {
+       sUsuario = $("#usuario").val();
+       sContraseña = $("#contraseña").val();
+       $.ajax({
+           url: 'http://itculiacan.edu.mx/dadm/apipaselista/data/validausuario.php?usuario='+sUsuario+'&clave='+sContraseña,
+           dataType: 'json',
+           success: function(data)
+           {
+                bRespuesta = data.respuesta;
+                sUsuarioValida = data.usuariovalida;
+                sPeriodo = data.periodoactual;
+                if (bRespuesta)
+                {
+                    alert("correcto");
+                }
+           }
+       });
+   }
+    $('#btnIngresar').on("click",validarLogueo);
 }
+$(document).ready(inicia);
