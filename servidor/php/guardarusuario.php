eeb19899e6edcf43830e45aef7f573d3b323e38c
CCDM05 <yonamg67@gmail.com>
2018-05-30 11:22:41 -0700

    Ya casi guarda

--- servidor/php/guardarusuario.php
+++ servidor/php/guardarusuario.php
@@ -38,29 +38,4 @@ function guardarusuario(){
 			break;
 	}
 
-
-	function 	($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
-{
-  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
 ?>
